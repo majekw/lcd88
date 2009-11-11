6a78699658e00ce80436c5e57f53c969c13752d6
majekw <github@mamy.to>
2009-11-11 15:57:12 +0000

    przerobione modele na nowy uklad kanalow

--- models.asm
+++ models.asm
@@ -8,7 +8,7 @@
 ; 25 - 1
 ; 26 - -1
 ; 27 - buzzer
-; 28-31 - dedicated channels for trims for sticks 1-4 (updated automatically from extender)
+; 28-31 - dedicated channels for trims for sticks 1-4 (can be updated automatically from extender)
 
 ;
 ; containter definition:
@@ -81,6 +81,7 @@
 .cseg
 
 ; basic 4 ch model - no mixers, just dumb way to copy sticks to output channels
+;	with comments to each channels (nice example)
 ;
 .set		model=1
 		;channels
@@ -92,28 +93,28 @@
 		.db	model+(2<<6),6,17,6,0,0		;ch1 out
 		.db	model+(2<<6),6,18,7,0,0		;ch2 out
 		.db	model+(2<<6),6,19,8,0,0		;ch3 out
-		.db	model+(2<<6),6,27,9,0,0		;ch0 trim (default 0)
-		.db	model+(2<<6),6,28,10,0,0	;ch1 trim
-		.db	model+(2<<6),6,29,11,0,0	;ch2 trim
-		.db	model+(2<<6),6,30,12,0,0	;ch3 trim
-		.db	model+(2<<6),6,31,13,0,4	;ch0 reverse (default 1 = no reverse)
-		.db	model+(2<<6),6,32,14,0,4	;ch1 reverse (default 1 = no reverse)
-		.db	model+(2<<6),6,33,15,0,4	;ch2 reverse (default 1 = no reverse)
-		.db	model+(2<<6),6,34,16,0,4	;ch3 reverse (default 1 = no reverse)
-		.db	model+(2<<6),6,35,0,0,0	;ch0 connection between trim and inverse
-		.db	model+(2<<6),6,36,0,0,0	;ch1 connection between trim and inverse
-		.db	model+(2<<6),6,37,0,0,0	;ch2 connection between trim and inverse
-		.db	model+(2<<6),6,38,0,0,0	;ch3 connection between trim and inverse
+		.db	model+(2<<6),6,28,9,0,0		;ch0 trim (default 0)
+		.db	model+(2<<6),6,29,10,0,0	;ch1 trim
+		.db	model+(2<<6),6,30,11,0,0	;ch2 trim
+		.db	model+(2<<6),6,31,12,0,0	;ch3 trim
+		.db	model+(2<<6),6,32,13,0,4	;ch0 reverse (default 1 = no reverse)
+		.db	model+(2<<6),6,33,14,0,4	;ch1 reverse (default 1 = no reverse)
+		.db	model+(2<<6),6,34,15,0,4	;ch2 reverse (default 1 = no reverse)
+		.db	model+(2<<6),6,35,16,0,4	;ch3 reverse (default 1 = no reverse)
+		.db	model+(2<<6),6,36,0,0,0	;ch0 connection between trim and inverse
+		.db	model+(2<<6),6,37,0,0,0	;ch1 connection between trim and inverse
+		.db	model+(2<<6),6,38,0,0,0	;ch2 connection between trim and inverse
+		.db	model+(2<<6),6,39,0,0,0	;ch3 connection between trim and inverse
 
 		;blocks
-		.db	model+(0<<6),10,1,21,1,2,1,0,27,35	;trim for ch0
-		.db	model+(0<<6),10,2,22,1,2,1,1,28,36	;trim for ch1
-		.db	model+(0<<6),10,3,23,1,2,1,2,29,37	;trim for ch2
-		.db	model+(0<<6),10,4,24,1,2,1,3,30,38	;trim for ch3
-		.db	model+(0<<6),10,5,25,2,2,1,35,31,16	;reverse for ch0
-		.db	model+(0<<6),10,6,26,2,2,1,36,32,17	;reverse for ch1
-		.db	model+(0<<6),10,7,27,2,2,1,37,33,18	;reverse for ch2
-		.db	model+(0<<6),10,8,28,2,2,1,38,34,19	;reverse for ch3
+		.db	model+(0<<6),10,1,21,1,2,1,0,28,36	;trim for ch0
+		.db	model+(0<<6),10,2,22,1,2,1,1,29,37	;trim for ch1
+		.db	model+(0<<6),10,3,23,1,2,1,2,30,38	;trim for ch2
+		.db	model+(0<<6),10,4,24,1,2,1,3,31,39	;trim for ch3
+		.db	model+(0<<6),10,5,25,2,2,1,36,32,16	;reverse for ch0
+		.db	model+(0<<6),10,6,26,2,2,1,37,33,17	;reverse for ch1
+		.db	model+(0<<6),10,7,27,2,2,1,38,34,18	;reverse for ch2
+		.db	model+(0<<6),10,8,28,2,2,1,39,35,19	;reverse for ch3
 		
 		;block processing order
 		.db	model+(1<<6),10,1,2,3,4,5,6,7,8
@@ -145,36 +146,50 @@
 		.db	model+(3<<6),20,27,"reverse for ch3",0,0
 		.db	model+(3<<6),20,28,"reverse for ch4",0,0
 
-; simple 2 channel delta - minimalistic scenario (without delta special block)
-; ch0 --trim--ch34--reverse--ch35--mul--ch36---add---out0
-;       ch27         ch29          ch31      \   \---------\
+; simple 2 channel delta - minimalistic scenario without descriptions (and made without delta special block)
+; ch0 --trim--ch32--reverse--ch34--mul--ch36---add---out0
+;       ch28         ch33          ch35      \   \---------\
 ;                                             \            |
-; ch1 --trim--ch37--reverse--ch38--mul--ch39--add---out1   |
-;       ch28         ch30          ch32   \                |
-;                                          -neg--ch40------/
-;                          
-; ch27=0	ch28=0
-; ch29=1	ch30=1
-; ch31=0.5	ch32=0.5
+; ch1 --trim--ch37--reverse--ch39--mul--ch41--add---out1   |
+;       ch29         ch38          ch40   \                |
+;                                          -neg--ch42------/
+;
+; ch2 --trim--ch43--reverse--out2
+;       ch30         ch44
+;
+; ch3 --trim--ch45--reverse--out2
+;       ch31         ch46
+;
+; ch28=0	ch29=0
+; ch33=1	ch38=1
+; ch35=0.5	ch40=0.5
 .set	model=2
 		;channels
-		.db	model+(2<<6),6,27,0,0,0		;ch0 trim (default 0)
-		.db	model+(2<<6),6,28,0,0,0		;ch1 trim
-		.db	model+(2<<6),6,29,0,0,4		;ch0 reverse (default 1 = no reverse)
-		.db	model+(2<<6),6,30,0,0,4		;ch1 reverse (default 1 = no reverse)
-		.db	model+(2<<6),6,31,0,0,2		;ch0 x0.5
-		.db	model+(2<<6),6,32,0,0,2		;ch1 x0.5
+		.db	model+(2<<6),6,28,0,0,0		;ch0 trim (default 0)
+		.db	model+(2<<6),6,29,0,0,0		;ch1 trim
+		.db	model+(2<<6),6,30,0,0,0		;ch2 trim
+		.db	model+(2<<6),6,31,0,0,0		;ch3 trim
+		.db	model+(2<<6),6,33,0,0,4		;ch0 reverse (default 1 = no reverse)
+		.db	model+(2<<6),6,38,0,0,4		;ch1 reverse (default 1 = no reverse)
+		.db	model+(2<<6),6,35,0,0,2		;ch0 x0.5
+		.db	model+(2<<6),6,40,0,0,2		;ch1 x0.5
+		.db	model+(2<<6),6,44,0,0,4		;ch2 reverse (default 1 = no reverse)
+		.db	model+(2<<6),6,46,0,0,4		;ch3 reverse (default 1 = no reverse)
 		;blocks
-		.db	model+(0<<6),10,1,0,1,2,1,0,27,34	;trim for ch0
-		.db	model+(0<<6),10,2,0,1,2,1,1,28,37	;trim for ch1
-		.db	model+(0<<6),10,3,0,2,2,1,34,29,35	;rev for ch0
-		.db	model+(0<<6),10,4,0,2,2,1,37,30,38	;rev for ch1
-		.db	model+(0<<6),10,5,0,4,2,1,35,31,36	;mul for ch0
-		.db	model+(0<<6),10,6,0,4,2,1,38,32,39	;mul for ch1
-		.db	model+(0<<6),10,7,0,12,2,1,36,40,16	;add for ch0
-		.db	model+(0<<6),10,8,0,12,2,1,36,39,17	;add for ch1
-		.db	model+(0<<6),10,9,0,15,1,1,39,40,0	;neg for inverse ch1
+		.db	model+(0<<6),10,1,0,1,2,1,0,28,32	;trim for ch0
+		.db	model+(0<<6),10,2,0,1,2,1,1,29,37	;trim for ch1
+		.db	model+(0<<6),10,3,0,2,2,1,32,33,34	;rev for ch0
+		.db	model+(0<<6),10,4,0,2,2,1,37,38,39	;rev for ch1
+		.db	model+(0<<6),10,5,0,4,2,1,34,35,36	;mul for ch0
+		.db	model+(0<<6),10,6,0,4,2,1,39,40,41	;mul for ch1
+		.db	model+(0<<6),10,7,0,12,2,1,36,42,16	;add for ch0
+		.db	model+(0<<6),10,8,0,12,2,1,36,41,17	;add for ch1
+		.db	model+(0<<6),10,9,0,15,1,1,41,42,0	;neg for inverse ch1
+		.db	model+(0<<6),10,10,0,1,2,1,2,30,43	;trim for ch2
+		.db	model+(0<<6),10,11,0,1,2,1,3,31,45	;trim for ch3
+		.db	model+(0<<6),10,12,0,2,2,1,43,44,18	;rev for ch2
+		.db	model+(0<<6),10,13,0,2,2,1,45,46,19	;rev for ch03
 		;decription
 		.db	model+(3<<6),14,0,"Delta 2CH",0,0
 		;block processing order
-		.db	model+(1<<6),12,1,2,3,4,5,6,8,9,7,0
+		.db	model+(1<<6),16,1,2,3,4,5,6,8,9,7,10,11,12,13,0
